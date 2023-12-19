d6662d081e14bee0fff39dcb413d0d9cae9c89cd
csi22-irodhan <ivanrodriiguez04@gmail.com>
2023-12-19 13:34:59 +0100

    19/12/2023

---
+++ Controladores/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ejercicio2Array 
+{
+    /// <summary>
+    /// Clase principal de la aplicacion
+    /// 19/12/2023 -> irodhan
+    /// </summary>
+    class Program 
+    {
+        /// <summary>
+        /// Meotodo principal de la aplicacion
+        /// 19/12/2023 -> irodhan
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            char[] vocales = { 'a', 'e', 'i', 'o', 'u' };
+            char[] mezcla = { 'a', 'e', 'b', 'o', 'u' };
+
+            for (int i = 0; i < vocales.Length; i++) 
+            {
+                if (vocales[i] != mezcla[i]) 
+                { 
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
